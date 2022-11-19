4ce8d51c6537a47538512ac2c16063e4c3aac96b
CIOSAI <jyinteractive.tw@gmail.com>
2022-11-19 12:19:24 +0800

    new palette

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -18,11 +18,13 @@
 </script>
 
 <div class="counter">
-	<button on:click={() => (count -= 1)} aria-label="Decrease the counter by one">
-		<svg aria-hidden="true" viewBox="0 0 1 1">
-			<path d="M0,0.5 L1,0.5" />
-		</svg>
-	</button>
+  <div class="button-wrap">
+    <button on:click={() => (count -= 1)} aria-label="Decrease the counter by one">
+      <svg aria-hidden="true" viewBox="0 0 1 1">
+        <path d="M0,0.5 L1,0.5" />
+      </svg>
+    </button>
+  </div>
 
 	<div class="counter-viewport">
 		<div class="counter-digits" style="transform: translate(0, {100 * offset}%)">
@@ -31,36 +33,51 @@
 		</div>
 	</div>
 
-	<button on:click={() => (count += 1)} aria-label="Increase the counter by one">
-		<svg aria-hidden="true" viewBox="0 0 1 1">
-			<path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1" />
-		</svg>
-	</button>
+  <div class="button-wrap">
+    <button on:click={() => (count += 1)} aria-label="Increase the counter by one">
+      <svg aria-hidden="true" viewBox="0 0 1 1">
+        <path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1" />
+      </svg>
+    </button>
+  </div>
 </div>
 
 <style>
 	.counter {
 		display: flex;
-		border-top: 1px solid rgba(0, 0, 0, 0.1);
-		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
 		margin: 1rem 0;
 	}
 
-	.counter button {
+  .counter .button-wrap{
 		width: 2em;
+		padding: 0;
+		border-left: .1em solid var(--darken-color);
+		border-bottom: .1em solid var(--darken-color);
+		border-right: .1em solid var(--secondary-color);
+		border-top: .1em solid var(--secondary-color);
+    border-radius: .2em;
+    box-shadow: -.1em .1em .2em rgba(0,0,0,0.2);
+		font-size: 2rem;
+  }
+
+	.counter button {
+		width: 100%;
+    height: 100%;
 		padding: 0;
 		display: flex;
 		align-items: center;
 		justify-content: center;
 		border: 0;
-		background-color: transparent;
+    border-radius: .1em;
 		touch-action: manipulation;
+		background-color: var(--tertiary-color);
+    box-shadow: .2em -.2em .8em rgba(255, 255, 255, 0.3) inset;
 		color: var(--text-color);
 		font-size: 2rem;
 	}
 
-	.counter button:hover {
-		background-color: var(--secondary-color);
+	.counter .button-wrap:active {
+    box-shadow: -.02em .02em .2em rgba(0,0,0,0.2);
 	}
 
 	svg {
